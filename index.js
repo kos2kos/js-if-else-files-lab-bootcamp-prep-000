d02aef6b01924a80b504a00902e7b092f31779af
Nkosi Enciso Givhan <koasterman@gmail.com>
2019-04-03 11:15:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,4 +11,8 @@ if (name === "Susan") {
 } else { 
   console.log("Expected the name to be Susan")
 }
-if (height === "70") 
+if (height === "70") { 
+  console.log(¨the height is correct")
+} else {
+  console.log(expected "70")
+}
